7f2a45c49ef4242774ceb4432c6b0872ffb928de
wakana.tatsuta <caramelribon137@gmail.com>
2021-11-24 13:27:15 +0000

    registration form create

--- src/components/ShelfModal.vue
+++ src/components/ShelfModal.vue
@@ -12,6 +12,48 @@
       hide-footer
       title="書籍の登録"
     >
+      <b-form>
+        <b-form-group class="mb-4">
+          <label for="title">タイトル</label>
+          <b-form-input
+          id="title"
+          v-model="title"
+          required
+          type="text"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group class="mb-4">
+          <label for="file-uploader">表紙画像</label>
+          <b-form-file
+            id="file-uploader"
+            v-modal="imageFile"
+            accept="image/*"
+            browse-text="参照"
+            class="mb-2"
+            drop-placeholder="ファイルをここにドラッグ&ドロップしてください"
+            placeholder="画像ファイルを選択してください"
+            required
+          >
+          </b-form-file>
+          <b-img
+            :blank="!imageFileEncoded"
+            blank-color="#ccc"
+            height="80"
+            :src="imageFileEncoded"
+            alt="アップロード画像"
+          >
+          </b-img>
+        </b-form-group>
+        <b-button
+          block
+          class="mt-3"
+          type="submit"
+          variant="outline-success"
+        >
+          保存する
+        </b-button>
+      </b-form>
     </b-modal>
   </div>
 </template>
@@ -19,18 +61,61 @@
 <script>
 import {
   BButton,
+  BForm,
+  BFormFile,
+  BFormGroup,
+  BFormInput,
   BIconBookmarkPlus,
+  BImg,
   BModal,
   VBModal,
 } from 'bootstrap-vue';
 
+const encodeImage = (file) => {
+  const reader = new FileReader();
+  return new Promise((resolve) => {
+    reader.onloadend = (event) => resolve(event.target.result);
+    reader.readAsDataURL(file);
+  });
+};
+
 export default {
   components: {
     BButton,
+    BForm,
+    BFormFile,
+    BFormGroup,
+    BFormInput,
     BIconBookmarkPlus,
+    BImg,
     BModal,
   },
   directives: { 'b-modal': VBModal },
+  data() {
+    return {
+      imageFile: null,
+      imageFileEncoded: '',
+      isValidImageFile: null,
+      title: '',
+    };
+  },
+  watch: {
+    imageFile() {
+      if (this.imageFile) {
+        encodeImage(this.imageFile)
+          .then((res) => {
+            this.imageFileEncoded = res;
+            this.isValidImageFile = true;
+          })
+          .catch((err) => {
+            this.isValidImageFile = false;
+            console.error(err.message);
+          });
+      } else {
+        this.isValidImageFile = null;
+      }
+    },
+  },
 };
 </script>
 
