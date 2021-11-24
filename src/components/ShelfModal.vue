3021dc228596bb2610739e9a815b3df29754b3dd
wakana.tatsuta <caramelribon137@gmail.com>
2021-11-24 11:46:59 +0000

    modal button open/close

--- src/components/ShelfModal.vue
+++ src/components/ShelfModal.vue
@@ -3,9 +3,16 @@
     <b-button
       class="book-registration__show-btn"
       variant="outline-success"
+      v-b-modal.book-registration
     >
       <b-icon-bookmark-plus></b-icon-bookmark-plus> 書籍の登録
     </b-button>
+    <b-modal
+      id="book-registration"
+      hide-footer
+      title="書籍の登録"
+    >
+    </b-modal>
   </div>
 </template>
 
@@ -13,13 +20,17 @@
 import {
   BButton,
   BIconBookmarkPlus,
+  BModal,
+  VBModal,
 } from 'bootstrap-vue';
 
 export default {
   components: {
     BButton,
     BIconBookmarkPlus,
+    BModal,
   },
+  directives: { 'b-modal': VBModal },
 };
 </script>
 
